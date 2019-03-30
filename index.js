a5456eed60d992698ca09dabff4b8acd3c2d2e92
Jason Beedle <jason_beedle@hotmail.com>
2019-03-30 10:51:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-// add solution here
-function theBeatlesPlay(musicians, instruments); {
-  var array[];
-  for (var i = 0; i < musicians.length; i++) {
-    array.push(musicians[i] + ' plays ' + ' instruments ');
-  }
-  return array;
-}
+
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (var i = 0; i < musicians.length; i++) {
